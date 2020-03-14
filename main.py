58093865ae96bcbf4cd29df8b05e3d551d7a5280
heaust <heaust.azure@gmail.com>
2020-03-14 16:41:06 +0530

    installed pep8 formatter

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
-from time import  sleep
+from time import sleep
 from config import *
 from os import path
-script_path = path.dirname(path.abspath( __file__ ))
+script_path = path.dirname(path.abspath(__file__))
 
-class outlookBot():
+
+class OutlookBot():
     def __init__(self):
-        self.driver = webdriver.Firefox(executable_path=script_path+r'/geckodriver')
-    
+        self.driver = webdriver.Firefox(
+            executable_path=script_path+r'/geckodriver')
+
     def login(self):
         self.driver.get('https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=13&ct=1584179991&rver=7.0.6737.0&wp=MBI_SSL&wreply=https%3a%2f%2foutlook.live.com%2fowa%2f%3fnlp%3d1%26RpsCsrfState%3dab9b6d22-4a43-52a9-57c7-f37ccfd96ebd&id=292841&aadredir=1&CBCXT=out&lw=1&fl=dob%2cflname%2cwld&cobrandid=90015')
         next_btn = self.driver.find_element_by_xpath('//*[@id="idSIButton9"]')
@@ -21,5 +23,6 @@ class outlookBot():
         sign_btn = self.driver.find_element_by_xpath('//*[@id="idSIButton9"]')
         sign_btn.click()
 
-bot = outlookBot()
+
+bot = OutlookBot()
 bot.login()
